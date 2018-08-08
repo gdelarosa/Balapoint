f3aac144ba8f25115114fd4a3bed139aced2c82e
Gina <g26delarosa@gmail.com>
2018-08-08 13:00:55 -0700

    Initial Commit

---
+++ JunderaTests/JunderaTests.swift
@@ -0,0 +1,36 @@
+//
+//  JunderaTests.swift
+//  JunderaTests
+//
+//  Created by Gina De La Rosa on 8/8/18.
+//  Copyright © 2018 Gina De La Rosa. All rights reserved.
+//
+
+import XCTest
+@testable import Jundera
+
+class JunderaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
